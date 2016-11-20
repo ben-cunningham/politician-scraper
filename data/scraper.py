9648278c675a8497baa1685cd9dfa447906f4da5
ben-cunningham <bencunningham17@gmail.com>
2016-11-20 13:22:11 -0800

    Initial commit of scraper

---
+++ data/scraper.py
@@ -0,0 +1,10 @@
+WIKI_URL_21 = 'https://en.wikipedia.org/w/index.php?title=Category:21stj-century_American_politicians'
+WIKI_URL_20 = 'https://en.wikipedia.org/w/index.php?title=Category:20th-century_American_politicians'
+
+urls = []
+
+def scrape(url):
+    pass
+
+if __name__ == 'main':
+    scrape()
