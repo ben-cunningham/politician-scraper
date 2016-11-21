51f1ace91d6e51cd919718f0daa055bc38ad71e6
ben-cunningham <bencunningham17@gmail.com>
2016-11-21 11:54:44 -0800

    Fix wiki url

--- data/name_scraper.py
+++ data/name_scraper.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib
 
-WIKI_URL_21 = 'https://en.wikipedia.org/w/index.php?title=Category:21stj-century_American_politicians'
+WIKI_URL_21 = 'https://en.wikipedia.org/w/index.php?title=Category:21st-century_American_politicians'
 WIKI_URL_20 = 'https://en.wikipedia.org/w/index.php?title=Category:20th-century_American_politicians'
 
 urls= []
