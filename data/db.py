8b300c47a58595aa1d82940f747c9abfaf292843
ben-cunningham <bencunningham17@gmail.com>
2016-12-23 10:42:23 -0800

    Use class for db interface

--- data/db.py
+++ data/db.py
@@ -5,30 +5,35 @@ import urlparse
 
 from queries import Queries
 
-conn = None
+class DB():
+    
+    def __init__(self):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        url = config.get('Database config', 'DATABASE_URL')
+    
+        urlparse.uses_netloc.append("postgres")
+        url = urlparse.urlparse(url)
+    
+        self.conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+    
+        self.create_tables()
+        self.conn.commit()
+        # self.conn.close() 
+    
+    def create_tables(self):
+        cur = self.conn.cursor()
+        cur.execute(Queries.create_edge)
+        cur.execute(Queries.create_vertex)
+        cur.close()
 
-def create_tables():
-    cur = conn.cursor()
-    cur.execute(Queries.create_edge)
-    cur.execute(Queries.create_vertex)
-    cur.close()
-    
-if __name__ == '__main__':
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    url = config.get('Database config', 'DATABASE_URL')
-    
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(url)
-    
-    conn = psycopg2.connect(
-        database=url.path[1:],
-        user=url.username,
-        password=url.password,
-        host=url.hostname,
-        port=url.port
-    )
-    
-    create_tables()
-    conn.commit()
-    conn.close() 
+    def insert_politician(self, politician):
+        cur = self.conn.cursor()
+        cur.execute(Queries.insert_politician)
+        cur.close()
