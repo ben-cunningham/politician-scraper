34c2eaa5b88aff3edac73151fbbf1451e6962e54
ben-cunningham <bencunningham17@gmail.com>
2016-12-22 20:47:41 -0800

    Add datebase connection

--- data/db.py
+++ data/db.py
@@ -1,17 +1,31 @@
 import os.path
 import psycopg2
 import configparser
+import urlparse
 
-connection = None
+conn = None
 
 def create_tables():
-    pass
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+    cur.close()
     
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config.ini')
     url = config.get('Database config', 'DATABASE_URL')
-    print url
-    # create_table()
-    # data = get_info()
     
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(url)
+    
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    
+    create_tables()
+    conn.commit()
+    conn.close() 
