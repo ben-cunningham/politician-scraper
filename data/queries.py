f67e75ace5dcc813679cb2c6300b4616d56ee534
ben-cunningham <bencunningham17@gmail.com>
2016-12-22 22:42:18 -0800

    Execute create table statments

--- data/queries.py
+++ data/queries.py
@@ -1,1 +1,14 @@
-queries = {}
+class Queries:
+
+    create_edge = """
+    create table edge (
+        "to" int,
+        "from" int
+    );
+    """
+    
+    create_vertex = """
+    create table vertex(
+        name char(100)
+    );
+    """
