a18375da925d6fc7a862947370a30ae3d7b2acfe
ben-cunningham <bencunningham17@gmail.com>
2017-02-14 21:53:49 -0800

    Read url and add in BeautifulSoup

--- data/connection_scraper.py
+++ data/connection_scraper.py
@@ -1,9 +1,16 @@
 from db import DB
+from bs4 import BeautifulSoup
+import urllib
 
 db = DB()
 
-def scrape(page):
+def scrape_page(url):
+    response = urllib.urlopen(url)
+
+def scrape():
     rows = db.get_rows()
+    for row in rows:
+        scrape_page(row[2])
 
 if __name__ == '__main__':
-    scrape(None)
+    scrape()
