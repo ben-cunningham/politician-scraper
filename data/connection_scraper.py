c798119aac73bd9aaf7508524fb7e22a12a84639
ben-cunningham <bencunningham17@gmail.com>
2017-02-20 19:34:17 -0800

    Add basic scraping logic

--- data/connection_scraper.py
+++ data/connection_scraper.py
@@ -1,11 +1,35 @@
 from db import DB
-from bs4 import BeautifulSoup
 import urllib
+import re
+from bs4 import BeautifulSoup
+from text_parser import TextParser
 
 db = DB()
+parser = TextParser()
+
+def is_politician(url):
+    return True
+
+def get_sentances(p):
+    return []
 
 def scrape_page(url):
     response = urllib.urlopen(url)
+    soup = BeautifulSoup(response, 'html.parser')
+    soup = soup.find('div', {'id': 'bodyContent'})
+    for script in soup(["script", "style"]):
+        script.extract()
+
+    for p in soup.find_all('p'):
+        sentances = get_sentances(p)
+        for s in sentances:
+            for a in s.find_all('a'):
+                href = a['href']
+                name = re.match(r'/wiki/(\w+)', href)
+                if name:
+                    if is_politician(name):
+                        cat = parser.classify(s.getText())
+                        # insert connection into database here
 
 def scrape():
     rows = db.get_rows()
